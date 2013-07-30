83390d3d8dd856f556dc796ce869ef1a7ad42779
pchanxxx <p.chanxxx@googlemail.com>
2013-07-30 15:57:49 +0200

    change site name

--- fabfile.py
+++ fabfile.py
@@ -14,9 +14,9 @@ env.port = '22222'
 env.user = 'root'
 env.hosts = ['6zu4']
 env.webserver = '/opt/webserver/buildout.webserver'
-env.code_root = '/opt/sites/plonesite/buildout.plonesite'
+env.code_root = '/opt/sites/kita/buildout.kita'
 env.local_root = '/Users/anjaradtke/vw-devel/current/kita_beta/buildout.kita'
-env.sitename = 'plonesite'
+env.sitename = 'kita'
 env.code_user = 'root'
 env.prod_user = 'www'
 
