efa604b07d90dd37f8b300501328678bfe35bbdf
Serge Davidov <sd@kreativkombinat.de>
2014-05-23 16:51:11 +0200

    update plone version

--- fabfile.py
+++ fabfile.py
@@ -12,23 +12,43 @@ env.use_ssh_config = True
 env.forward_agent = True
 env.port = '22222'
 env.user = 'root'
-env.hosts = ['5zu2']
+env.hosts = ['z11']
 env.webserver = '/opt/webserver/buildout.webserver'
 env.code_root = '/opt/sites/kita/buildout.kita'
-env.local_root = '/Users/cb/dev/pg/buildout.kita'
+env.local_root = '/Users/sd/dev/kita/buildout.kita'
 env.sitename = 'kita'
 env.code_user = 'root'
 env.prod_user = 'www'
 
 
 @task
+def bo_conf():
+    #t = cuisine.file_local_read('packages.cfg')
+    filename = '%s/packages.cfg' % env.local_root
+    with open(filename, 'r+a') as f:
+        read_data = f.read()
+        # config = ConfigParser.ConfigParser()
+        print read_data
+
+
+def ls():
+    """ Low level configuration test """
+    with cd(env.code_root):
+        run('ls')
+
+
+def supervisorctl(*cmd):
+    """Runs an arbitrary supervisorctl command."""
+    with cd(env.webserver):
+        run('bin/supervisorctl ' + ' '.join(cmd))
+
+
 def deploy():
     """ Deploy current master to production server """
     project.site.update()
-    project.site.restart()
+    project.site.estart()
 
 
-@task
 def deploy_full():
     """ Deploy current master to production and run buildout """
     project.site.update()
@@ -36,15 +56,8 @@ def deploy_full():
     project.site.restart()
 
 
-@task
 def rebuild():
     """ Deploy current master and run full buildout """
     project.site.update()
     project.site.build_full()
     project.site.restart()
-
-
-@task
-def get_data():
-    """ Copy live database for local development """
-    project.db.download_data()
